2dee9e6f8550211b399906c34c0cbfaa01cd3629
Nadav Gover <nadavgover@gmail.com>
2019-09-11 12:03:46 +0300

    fixed

--- Sudoku.py
+++ Sudoku.py
@@ -218,6 +218,8 @@ class Sudoku:
         cell = self.get_min_possibilities_cell()
         if len(self.possibilities[cell]) == 1:
             return False
+        if len(self.possibilities[cell]) == 0:
+            return False
         return True
 
     def get_num_to_guess(self, min_cell, i):
@@ -308,5 +310,5 @@ if __name__ == '__main__':
                         [0, 5, 0, 0, 0, 7, 0, 0, 0], [0, 0, 0, 0, 4, 5, 7, 0, 0], [0, 0, 0, 1, 0, 0, 0, 3, 0],
                         [0, 0, 1, 0, 0, 0, 0, 6, 8], [0, 0, 8, 5, 0, 0, 0, 1, 0], [0, 9, 0, 0, 0, 0, 4, 0, 0]]
 
-    main(sudoku=sudoku)
+    main(sudoku=sudoku_that_the_program_cant_solve)
 
